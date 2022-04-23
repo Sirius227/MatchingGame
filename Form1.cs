eb46bba342a312c5dea7ed3c228d27673210e59e
Sirius227 <76209670+Sirius227@users.noreply.github.com>
2022-04-23 04:06:01 +0300

    Gereksiz namespace'leri kaldırma

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -66,7 +61,7 @@ namespace MatchingGame
             Group();
         }
 
-        Random random = new Random();
+        readonly Random random = new Random();
     
         void Group()
         {
